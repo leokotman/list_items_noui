b9bf7cc27f564ab8b3eb7d30c74698c2b33a6355
levkotman <leoktmn@yandex.ru>
2021-12-17 02:19:25 +0100

    started creating lists

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,48 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <create-list :lists="lists" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import CreateList from "./components/CreateList.vue";
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { CreateList },
+  data() {
+    return {
+      lists: [
+        {
+          title: "List 1",
+          items: [
+            { name: "Item 1", quantity: 10, color: "#E90804", checked: true },
+            { name: "Item 2", quantity: 16, color: "#F4F709", checked: true },
+            { name: "Item 3", quantity: 40, color: "#148007", checked: true },
+            { name: "Item 1", quantity: 0, color: "#0004FF", checked: false },
+          ],
+          allChecked: false,
+          someChecked: true,
+        },
+        {
+          title: "List 2",
+          items: [
+            { name: "Item 1", quantity: 20, color: "#0BE904", checked: false },
+            { name: "Item 2", quantity: 26, color: "#09D6F6", checked: false },
+            { name: "Item 3", quantity: 50, color: "#F66309", checked: false },
+            { name: "Item 1", quantity: 0, color: "#4D09F6", checked: false },
+          ],
+          allChecked: false,
+          someChecked: false,
+        },
+      ],
+    };
   },
+  methods: {},
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/CreateList.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <li v-for="(list, i) in lists" :key="i">
+      <label>
+        {{ list.title }}
+        <input type="checkbox" />
+      </label>
+      <ul>
+        <li v-for="(item, i) in list.items" :key="i">
+          <input type="checkbox" :id="item.id" />
+          <label :for="item.id">
+            {{ item.name }}
+          </label>
+          {{ item.quantity }}
+          <input type="color" v-model="item.color" />
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "CreateList",
+  props: ["lists"],
+};
+</script>
+
+<style scoped></style>
