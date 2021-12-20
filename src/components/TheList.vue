ffefe9a599445e0bfa46c72d9b21672eea1fe0ef
levkotman <leoktmn@yandex.ru>
2021-12-20 06:36:02 +0100

    checkbox diff styles on someChecked/allChecked

--- src/components/TheList.vue
+++ src/components/TheList.vue
@@ -1,19 +1,21 @@
 <template>
-  <div>
+  <section>
     <img
       src="../assets/img/icons8-expand-arrow-50.png"
-      :class="'list_arrow' + (!list.itemsShown ? ' list_arrow-closed' : '')"
-      @click="list.itemsShown = !list.itemsShown"
+      :class="'list_arrow' + (!itemsShown ? ' list_arrow-closed' : '')"
+      @click="itemsShown = !itemsShown"
     />
-    <label :for="list.title + '_' + index">
+    <label :for="list.title + '_' + index" class="container">
       <input
         type="checkbox"
         :id="list.title + '_' + index"
         v-model="allChecked"
+        :class="checkBoxClass"
       />
+      <span class="checkmark"></span>
       {{ list.title }}
     </label>
-    <ul class="items_list" v-show="list.itemsShown">
+    <ul class="items_list" v-show="itemsShown">
       <li v-for="(item, i) in list.items" :key="i">
         <label
           :for="item.name + '_' + index + '_' + i"
@@ -36,7 +38,7 @@
         <input type="color" v-model="item.color" />
       </li>
     </ul>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -46,19 +48,20 @@ export default {
     return {
       allChecked: false,
       selectedItems: [],
+      someChecked: false,
+      itemsShown: false,
     };
   },
   methods: {
     selectAll() {
-      if (this.list.allChecked) {
-        // this.allChecked = true;
+      if (this.allChecked) {
         const selected = this.list.items.forEach((item) => {
           return (item.checked = true);
         });
         console.log("selectAll allChecked worked");
-        this.list.someChecked = false;
+        this.someChecked = false;
         this.selectedItems = selected;
-      } else if (this.list.someChecked) {
+      } else if (this.someChecked) {
         console.log("selectAll someChecked worked");
       } else {
         // this.allChecked = false;
@@ -72,15 +75,36 @@ export default {
       let checkedItemsArr = this.list.items.filter(
         (item) => item.checked == true
       );
-      if (checkedItemsArr.length === this.list.items.length) {
+      if (
+        checkedItemsArr.length === this.list.items.length &&
+        this.list.items.length > 0
+      ) {
         console.log("allChecked = true");
         return (this.allChecked = true);
       } else if (checkedItemsArr.length > 0) {
         console.log("list.someChecked = true");
-        return (this.list.someChecked = true), (this.allChecked = false);
+        return (this.someChecked = true), (this.allChecked = false);
       } else {
         console.log("allChecked = false");
-        return (this.allChecked = false), (this.list.someChecked = false);
+        return (this.allChecked = false), (this.someChecked = false);
+      }
+    },
+    countItemsInList() {
+      if (this.list.items.length > 0) {
+        return (this.itemsShown = true);
+      } else {
+        return (this.itemsShown = false);
+      }
+    },
+  },
+  computed: {
+    checkBoxClass: function () {
+      if (this.someChecked) {
+        return "someChecked";
+      } else if (this.allChecked) {
+        return "allChecked";
+      } else {
+        return "notChecked";
       }
     },
   },
@@ -89,11 +113,100 @@ export default {
       this.list.allChecked = !this.list.allChecked;
       this.selectAll();
     },
+    someChecked() {
+      this.list.someChecked = this.someChecked;
+    },
+  },
+  created() {
+    this.countCheckedItems();
+    this.countItemsInList();
   },
 };
 </script>
 
 <style scoped>
+section {
+  margin: 1rem 0 1rem;
+}
+
+.container {
+  display: inline-block;
+  position: relative;
+  padding-left: 2rem;
+  margin-left: 1rem;
+  margin-bottom: 0.8rem;
+  cursor: pointer;
+  font-size: 1.2rem;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+/* Hide the browser's default checkbox */
+.container input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+/* Create a custom checkbox */
+.checkmark {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 25px;
+  width: 25px;
+  background-color: #eee;
+  border-radius: 5px;
+}
+
+/* On mouse-over, add a grey background color */
+.container:hover input ~ .checkmark {
+  background-color: #ccc;
+}
+
+/* When the checkbox is checked, add a blue background */
+.container input:checked ~ .checkmark,
+.container input.someChecked ~ .checkmark {
+  background-color: #2196f3;
+}
+
+/* Create the checkmark/indicator (hidden when not checked) */
+.checkmark:after {
+  content: "";
+  position: absolute;
+  display: none;
+}
+/* Show the checkmark when checked */
+.container input:checked ~ .checkmark:after {
+  display: block;
+}
+
+/* Style the someChecked indicator */
+.container input.someChecked ~ .checkmark:after {
+  display: block;
+  left: 8px;
+  top: 8px;
+  border: solid white;
+  border-width: 5px;
+  border-radius: 50%;
+}
+/* Style the allChecked indicator */
+.container input.allChecked ~ .checkmark:after {
+  left: 9px;
+  top: 5px;
+  width: 5px;
+  height: 10px;
+  border: solid white;
+  border-width: 0 3px 3px 0;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+
 .items_list {
   list-style-type: none;
 }
